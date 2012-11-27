1e6362b53b5988dd755edc0d11218403195bc24c
cadorn <christoph@christophdorn.com>
2012-11-27 11:17:17 -0800

    initial implementation

--- plugin.js
+++ plugin.js
@@ -2,7 +2,29 @@
 
 exports.for = function(API, plugin) {
 
+	var streePath = null;
+
+	function findStreePath() {
+		if (streePath === null) {			
+	        return API.OS.which("stree").then(function(path) {
+	        	streePath = path;
+	        	if (!path) {
+                    API.TERM.stdout.writenl("  \0cyan(If you install `stree` http://www.SourcetreeApp.com (no affiliation with sourcemint)\0)");
+                    API.TERM.stdout.writenl("  \0cyan(these packages will be automatically opened for you to commit changes.\0)");
+                    API.TERM.stdout.writenl("");
+                    throw true;
+	        	}
+	        	return path;
+	        });
+		} else {
+			return API.Q.resolve(streePath);
+		}
+	}
 
 
+	plugin.fix = function(node, options) {
+		return findStreePath().then(function(streePath) {
+			return API.OS.exec(streePath + " " + node.path);
+		});
+	}
 }
-
